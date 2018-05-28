db77ca0154e852eddafb7dfb619240f7d9e91899
Fernando Kupka <nandokupka98@gmail.com>
2018-05-28 10:23:07 -0300

    Search

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,25 +3,33 @@ import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatInputModule} from '@angular/material/input';
 import { RouterModule, Routes } from '@angular/router';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
 import { SplashComponent } from './splash/splash.component';
+import { SearchResultComponent } from './search-result/search-result.component';
 
 const appRoutes: Routes = [
   { path: '', component: SplashComponent },
+  { path: 'search/:term', component: SearchResultComponent },
+  
 ];
 
 @NgModule({
   declarations: [
     SplashComponent,
-    AppComponent
+    AppComponent,
+    SearchResultComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     RouterModule.forRoot(appRoutes),
-    MatInputModule
+    MatInputModule,
+    MatAutocompleteModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/splash/splash.component.ts
+++ src/app/splash/splash.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import {startWith} from 'rxjs/operators/startWith';
+import {map} from 'rxjs/operators/map';
+import { FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-splash',
@@ -6,10 +11,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./splash.component.sass']
 })
 export class SplashComponent implements OnInit {
+  currentFilter: string;
+  options: string[];
+  filteredOptions: any[];
+  timeout: number;
+  isWaitingKeypress: boolean;
 
-  constructor() { }
+  constructor(private router: Router) {
+    this.options = []
+  }
 
   ngOnInit() {
   }
 
+  keyInput(ev) {
+    this.filteredOptions = []
+    console.log('oloook', this.currentFilter)
+    this.currentFilter = ev.target.value
+    this.isWaitingKeypress = true;
+    if (this.isWaitingKeypress) {
+      window.clearTimeout(this.timeout)
+    }
+    this.timeout = window.setTimeout(x => {
+      this.doFilter(this.currentFilter).then(res => {
+        this.filteredOptions = res;
+      })
+    }, 700)
+  }
+
+  doSearch() {
+    setTimeout(x => {
+      this.router.navigate(['', 'search', this.currentFilter])
+    }, 200)
+  }
+
+  doFilter(term: String) {
+    console.log('oi')
+    return new Promise<any[]>(function(resolve, reject) {
+      setTimeout(x => {
+        if (term.toUpperCase() == 'free'.toUpperCase())
+          resolve(['Free Bird', 'Free as Bird'])
+        else
+          resolve(['Teste', 'Teste'])
+        }, 100)
+    });
+  }
 }
